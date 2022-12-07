9e83bd98a417dc12472975603461be5bab033833
JacobDoucet <lawson.doucet@gmail.com>
2022-12-07 13:29:47 +0100

    Add files via upload

--- day7.py
+++ day7.py
@@ -1,8 +1,5 @@
 import re
 
-# Part 1
-max_size = 100000
-
 data = open("day7.txt").read().split("\n")
 
 navigate_down = "cd ([A-Za-z0-9]+)"
@@ -26,25 +23,16 @@ for row in data:
                 folders[path] = 0
             folders[path] += next_size
 
-total_size = 0
-total_size_under_limit = 0
-for path in filter(lambda p: folders[path] <= max_size, folders):
-    total_size_under_limit += folders[path]
+# Part 1
 
-print("Part 1:", total_size_under_limit)
+max_size = 100000
+total_size = 0
+print("Part 1:", sum(filter(lambda size: size <= max_size, map(lambda path: folders[path], folders))))
 
 # Part 2
 
 system_size = 70000000
 required_unused_size = 30000000
 system_max_used_size = system_size - required_unused_size
-
 min_free_up_space = folders["/"] - system_max_used_size
-# Start with the root folder. All child folders will be smaller
-size_to_remove = folders["/"]
-for path in folders:
-    if min_free_up_space < folders[path] < size_to_remove:
-        size_to_remove = folders[path]
-
-print("Part 2:", size_to_remove)
-
+print("Part 2:", min(list(filter(lambda size: min_free_up_space < size, map(lambda path: folders[path], folders)))))
